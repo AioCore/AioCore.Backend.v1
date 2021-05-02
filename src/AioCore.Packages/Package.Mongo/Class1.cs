6fba2ea8b135b974df9525e2f3eb5f447619ca3c
khoinguyenict <khoinguyenict@gmail.com>
2021-05-02 22:32:37 +0700

    update github action build and push with docker-compose

--- src/AioCore.Packages/Package.Mongo/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Package.Mongo
-{
-    public class Class1
-    {
-    }
-}
--- src/AioCore.Packages/Package.Redis/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Package.Redis
-{
-    public class Class1
-    {
-    }
-}
