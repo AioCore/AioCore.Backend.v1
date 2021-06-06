e353a3a1ae6a18a5d17ade6ec2bfb0f36fbde46d
KhoiNguyenICT <khoinguyenict@gmail.com>
2021-06-06 22:46:59 +0700

    update action settings

--- src/AioCore.Domain/SettingAggregatesModel/SettingComponentAggregate/SettingComponent.cs
+++ src/AioCore.Domain/SettingAggregatesModel/SettingComponentAggregate/SettingComponent.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using AioCore.Domain.SettingAggregatesModel.SettingFieldAggregate;
+﻿using AioCore.Domain.SettingAggregatesModel.SettingFieldAggregate;
 using AioCore.Shared.Seedwork;
+using System;
+using System.Collections.Generic;
 
 namespace AioCore.Domain.SettingAggregatesModel.SettingComponentAggregate
 {
@@ -34,6 +35,35 @@ namespace AioCore.Domain.SettingAggregatesModel.SettingComponentAggregate
             public DataType DataType { get; set; }
 
             public bool Hidden { get; set; }
+        }
+
+        public class ActionSettings
+        {
+            public string ActionName { get; set; }
+
+            // fontawesome: <i class="fad fa-acorn"></i>
+            public string Icon { get; set; }
+
+            public List<ActionStep> Steps { get; set; }
+
+            public enum ActionStepType
+            {
+                SaveBase = 1,
+                SaveRealtime
+            }
+
+            public enum ActionTriggerType
+            {
+                Client = 1,
+                Server
+            }
+
+            public class ActionStep
+            {
+                public ActionStepType StepType { get; set; }
+
+                public ActionTriggerType TriggerType { get; set; }
+            }
         }
     }
 }
