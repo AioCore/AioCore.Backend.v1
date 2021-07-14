de1327c773e91173a3aa55895c0abd0ff954c3ea
KhoiNguyenICT <khoinguyenict@gmail.com>
2021-07-14 15:34:27 +0700

    done for config tailwindcss

--- src/AioCore.Hosted/AioCore.Web/Program.cs
+++ src/AioCore.Hosted/AioCore.Web/Program.cs
@@ -12,7 +12,7 @@ namespace AioCore.Web
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
+            builder.Services.AddAntDesign();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
