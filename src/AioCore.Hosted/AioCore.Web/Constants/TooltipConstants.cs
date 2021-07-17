ee5c8bc1ce800b10fab6b34ba4120840053bf50b
khoinguyenict <khoinguyenict@gmail.com>
2021-07-17 22:37:27 +0700

    cấu hình danh sách icon chức năng menu

---
+++ src/AioCore.Hosted/AioCore.Web/Constants/TooltipConstants.cs
@@ -0,0 +1,15 @@
+namespace AioCore.Web.Constants
+{
+    public static class TooltipConstants
+    {
+        public const string HomeTooltip = "Trang chủ";
+        public const string TenantTooltip = "Thuê bao";
+        public const string EntityTooltip = "Đối tượng";
+        public const string FormTooltip = "Nhập liệu";
+        public const string ViewTooltip = "Hiển thị dữ liệu";
+        public const string FilterTooltip = "Bộ lọc";
+        public const string ReportTooltip = "Báo cáo";
+        public const string LayoutTooltip = "Bố cục";
+        public const string FeatureTooltip = "Cây chức năng";
+    }
+}
